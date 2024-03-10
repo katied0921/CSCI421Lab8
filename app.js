0e34d48b014de1c9ead5627152c2a9dc0231de47
katied0921 <kaitlyn.dowlin0921@gmail.com>
2024-03-10 18:19:14 -0400

    Got lab 3 stuff working

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 // Update app.js to load db.js file.
-require('./app_api/models/db');
+//require('./app_api/models/db');
 
 // We only need one router for routes and controllers architecture.
 var routes = require('./app_server/routes/index');
 // Set up router for API.
-var routesApi = require('./app_api/routes/index');
+//var routesApi = require('./app_api/routes/index');
 
 var app = express();
 
@@ -30,7 +30,7 @@ app.use('/icons', express.static(path.join(__dirname, 'node_modules/bootstrap-ic
 // We only need one router for routes and controllers architecture.
 app.use('/', routes);
 // Makes it so that application will only use API routes when the route starts with /api.
-app.use('/api', routesApi);
+//app.use('/api', routesApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
